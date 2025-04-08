fe1b8f88daec4417500f7bc4f783aafe46da5c75
emmanuelrgeek@gmail.com <emmanuelrgeek@gmail.com>
2025-04-08 12:55:31 -0300

    docker

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,11 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './app/views/**/*.{html,erb}',
-    './app/javascript/**/*.{js,jsx,ts,tsx}',  // Si usas JavaScript/JSX/TSX
-    './app/assets/stylesheets/**/*.{css,scss}',    './app/helpers/**/*.rb',
-    // Si usas SCSS o CSS
-  ],
+  purge: [ './pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}', ],
   theme: {
     extend: {},
   },
