518528dce250f7538cbe99cb7254c01543a2e1fe
emmanuelrgeek@gmail.com <emmanuelrgeek@gmail.com>
2025-04-08 14:49:28 -0300

    seed2

--- app/controllers/custom_posts_controller.rb
+++ app/controllers/custom_posts_controller.rb
@@ -1,14 +1,24 @@
 class CustomPostsController < ApplicationController
-
   def work
-    @experiences = JSON.parse(File.read(Rails.root.join('db','data', 'experience.json')))
+    file_path = Rails.root.join('db', 'data', 'experience.json')
+    if File.exist?(file_path)
+      @experiences = JSON.parse(File.read(file_path))
+    else
+      Rails.logger.error "Archivo de experiencias no encontrado en #{file_path}"
+      @experiences = []
+    end
   end
 
   def skills
-    @skills = JSON.parse(File.read(Rails.root.join('db','data', 'skills.json')))
+    file_path = Rails.root.join('db', 'data', 'skills.json')
+    if File.exist?(file_path)
+      @skills = JSON.parse(File.read(file_path))
+    else
+      Rails.logger.error "Archivo de habilidades no encontrado en #{file_path}"
+      @skills = []
+    end
   end
 
   def education
   end
-
 end
