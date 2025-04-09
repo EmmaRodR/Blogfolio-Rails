4bcf6b0626c914aacf0b1395a58589e5378f6575
emmanuelrgeek@gmail.com <emmanuelrgeek@gmail.com>
2025-04-09 15:17:39 -0300

    update0904

---
+++ app/javascript/controllers/keep_alive_controller.js
@@ -0,0 +1,23 @@
+import { Controller } from "@hotwired/stimulus";
+
+// Connects to data-controller="keep-alive"
+export default class extends Controller {
+  connect() {
+    this.startTimer();
+    console.log('KeepAlive conectado');
+  }
+
+
+  startTimer() {
+    setTimeout(()=> {
+      this.keepSessionAlive(); 
+      console.log('KeepAlive ejecutado');
+
+    }, 5 * 60 * 1000);
+  }
+
+  keepSessionAlive() {
+    window.location.href= "/home";
+  }
+
+}
